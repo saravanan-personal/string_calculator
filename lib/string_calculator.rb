acff024b82b1159e8ddf83eb12f7a132728f2b62
saravanan-personal <saravanan.wrk@gmail.com>
2024-09-01 10:58:11 +0530

    Code logic to raise exceptions for negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,7 +5,13 @@ require_relative "string_calculator/version"
 # Module to perform string calculations
 module StringCalculator
   class Error < StandardError; end
-  
+
+  def self.detect_negatives(numbers)
+    negatives = numbers.split(",").select { |number| number.to_i.negative? }
+    raise Error, "negatives not allowed: #{negatives.join(", ")}" if negatives.any?
+    numbers
+  end
+
   def self.detect_dynamic_delimiter(numbers)
     if numbers.start_with?("//")
       delimiter = numbers[2]
@@ -14,7 +20,7 @@ module StringCalculator
     end
     numbers
   end
-  
+
   def self.sanitize_input(numbers)
     numbers.gsub("\n", ",")
   end
@@ -22,8 +28,8 @@ module StringCalculator
   def self.sum(numbers)
     numbers.split(",").map(&:to_i).sum
   end
-  
+
   def self.add(numbers)
-    sum sanitize_input detect_dynamic_delimiter(numbers)
+    sum detect_negatives sanitize_input detect_dynamic_delimiter(numbers)
   end
 end
