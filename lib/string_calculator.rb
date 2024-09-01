6073c7ff1cd2a075f259207072f1bc2f0c611ad3
saravanan-personal <saravanan.wrk@gmail.com>
2024-09-01 10:44:26 +0530

    Refactoring...

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -6,12 +6,24 @@ require_relative "string_calculator/version"
 module StringCalculator
   class Error < StandardError; end
   
-  def self.add(numbers)
+  def self.detect_dynamic_delimiter(numbers)
     if numbers.start_with?("//")
       delimiter = numbers[2]
       numbers = numbers[4..-1]
       numbers.gsub!(delimiter, ",")
     end
-    numbers.gsub("\n", ",").split(",").map(&:to_i).sum
+    numbers
+  end
+  
+  def self.sanitize_input(numbers)
+    numbers.gsub("\n", ",")
+  end
+
+  def self.sum(numbers)
+    numbers.split(",").map(&:to_i).sum
+  end
+  
+  def self.add(numbers)
+    sum sanitize_input detect_dynamic_delimiter(numbers)
   end
 end
