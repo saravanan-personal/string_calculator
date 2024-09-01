25841bd8dd9ac77171b2b21f46688a9d68a88d4f
saravanan-personal <saravanan.wrk@gmail.com>
2024-09-01 10:36:00 +0530

    logic change to fetch delimiters from input string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -7,6 +7,11 @@ module StringCalculator
   class Error < StandardError; end
   
   def self.add(numbers)
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers[4..-1]
+      numbers.gsub!(delimiter, ",")
+    end
     numbers.gsub("\n", ",").split(",").map(&:to_i).sum
   end
 end
