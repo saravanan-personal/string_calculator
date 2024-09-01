36c4e4a25ef68708af8c7e0caef3411a688a716a
saravanan-personal <saravanan.wrk@gmail.com>
2024-09-01 11:43:28 +0530

    Test case to check with invalid input format

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe StringCalculator do
     it "returns 3 for '1,2'" do
       expect(StringCalculator.add("1,2")).to eq(3)
     end
-    
+
     context "it can handle n numbers" do
       it "returns 6 for '1,2,3'" do
         expect(StringCalculator.add("1,2,3")).to eq(6)
@@ -22,13 +22,13 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1,2,3,4")).to eq(10)
       end
     end
-    
+
     context "it can handle more than one digit" do
       it "returns 101 for '100,1'" do
         expect(StringCalculator.add("100,1")).to eq(101)
       end
     end
-    
+
     context "it can handle new lines" do
       it "returns 6 for '1\n2,3'" do
         expect(StringCalculator.add("1\n2,3")).to eq(6)
@@ -38,7 +38,7 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1\n2\n3,4")).to eq(10)
       end
     end
-    
+
     context "it supports dynamic delimiters" do
       it "returns 3 for '//;\n1;2'" do
         expect(StringCalculator.add("//;\n1;2")).to eq(3)
@@ -48,13 +48,13 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("//@\n1@2@3")).to eq(6)
       end
     end
-    
+
     context "it supports new line as dynamic delimiter" do
       it "returns 6 for '//\n\n1\n2\n3'" do
         expect(StringCalculator.add("//\n\n1\n2\n3")).to eq(6)
       end
     end
-    
+
     context "it raises an exception for negative numbers" do
       it "raises an exception for '-1'" do
         expect { StringCalculator.add("-1") }.to raise_error(StringCalculator::Error, "negatives not allowed: -1")
@@ -62,6 +62,16 @@ RSpec.describe StringCalculator do
 
       it "raises an exception for '-1,-2,3'" do
         expect { StringCalculator.add("-1,-2") }.to raise_error(StringCalculator::Error, "negatives not allowed: -1, -2")
+      end
+    end
+
+    context "it throws an exception for invalid input format" do
+      it "raises an exception for '1,2,3a'" do
+        expect { StringCalculator.add("1,2,3a") }.to raise_error(StringCalculator::Error, "invalid input format")
+      end
+
+      it "raises an exception for '//;\n1;;2;3'" do
+        expect { StringCalculator.add("//;\n1;;2;3") }.to raise_error(StringCalculator::Error, "invalid input format")
       end
     end
   end
